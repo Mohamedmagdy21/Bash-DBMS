a5c4c67123bf90aa70936bb9620644df90c65794
sherif <sherifgamal9441@gmail.com>
2025-11-25 20:37:54 +0200

    wrote table_drop.sh file logic

---
+++ functions/table_drop.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/bash
+
+#remove a table by entering its name
+
+while true; do
+    echo -n "Enter Table Name: "
+    read table_name
+
+    if [ -z "$table_name" ]; then
+        echo "Error: Table Name cannot be empty."
+    elif ! [[ "$table_name" =~ ^[a-zA-Z0-9_]+$ ]]; then
+        echo "Error: Invalid Table Name. No spaces or special characters allowed."
+    else
+        # Input is valid
+        break
+    fi
+done
+
+while true; do
+    echo -n "Are you sure you want to remove this table ?"
+    read intent
+
+    if [ $intent == "yes" ]; then
+		rm databases/student/$table_name
+        rm databases/student/$table_name.meta
+		break
+    else
+        # exit the code
+        break
+    fi
+done
